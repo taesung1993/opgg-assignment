e683e4cd31c82b2c8cd48da490502ec818deb68e
taesung1993 <cheonyulin@gmail.com>
2022-08-18 21:18:35 +0900

    add css properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        opgg: '1000px'
+      },
+      height: {
+        header: '6.0625rem'
+      },
+      colors: {
+        opgg: '#1ea1f7'
+      }
+    },
+    fontFamily: {
+      apple: ['AppleSDGothicNeo', 'Noto Sans KR', 'sans-serif'],
+      sans: ['Helvetica', 'Arial', 'sans-serif']
+    }
   },
-  plugins: [],
+  plugins: []
 };
