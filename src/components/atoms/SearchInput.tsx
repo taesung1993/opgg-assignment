888f1eb9b2d7cf589a860e27eb788da850567ef8
taesung1993 <cheonyulin@gmail.com>
2022-08-20 09:52:58 +0900

    add onSearch Event

--- src/components/atoms/SearchInput.tsx
+++ src/components/atoms/SearchInput.tsx
@@ -1,10 +1,28 @@
-import { useCallback, Dispatch } from 'react';
+import { useCallback, Dispatch, useState, ChangeEvent } from 'react';
+import { useRecoilState } from 'recoil';
+import States from '../../states';
 
 interface Props {
   setShowSearchContainer: Dispatch<boolean>;
 }
 
 export default function SearchInput({ setShowSearchContainer }: Props) {
+  const [_, setKeyword] = useRecoilState(States.SearchKeyword);
+  const onSearch = useCallback(async (event: ChangeEvent) => {
+    let waiting = false;
+    return (() => {
+      if (!waiting) {
+        waiting = true;
+        const target = event.target as HTMLInputElement;
+        const value = target.value;
+        setKeyword(value);
+        setTimeout(() => {
+          waiting = false;
+        }, 250);
+      }
+    })();
+  }, []);
+
   const onFocus = useCallback(() => {
     setShowSearchContainer(true);
   }, []);
@@ -15,6 +33,7 @@ export default function SearchInput({ setShowSearchContainer }: Props) {
       placeholder='소환사명,챔피언…'
       className='w-full h-full text-xs font-apple focus:outline-none'
       onFocus={onFocus}
+      onChange={onSearch}
     />
   );
 }
