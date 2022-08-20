4ade87fb76c6450f1c678f909408adf07678e327
taesung1993 <cheonyulin@gmail.com>
2022-08-20 20:24:39 +0900

    create Main and apply to Home component

---
+++ src/components/organisms/Main.tsx
@@ -0,0 +1,10 @@
+import Molecules from '../molecules';
+
+export default function Main() {
+  return (
+    <>
+      <Molecules.Profile />
+      <Molecules.SummonerInfo />
+    </>
+  );
+}
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,17 +1,10 @@
-import { Suspense } from 'react';
 import Templates from '../templates';
+import Organisms from '../organisms';
 
 export default function Home() {
   return (
     <Templates.Nested>
-      <Suspense
-        fallback={
-          <div style={{ width: '300px', height: '300px', background: 'red' }}>
-            hello
-          </div>
-        }>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      </Suspense>
+      <Organisms.Main />
     </Templates.Nested>
   );
 }
