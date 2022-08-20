9d347fd8ee739f9a580d36f1bcd968dcb0065167
taesung1993 <cheonyulin@gmail.com>
2022-08-20 20:24:13 +0900

    add profile and summonerinfo components

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,7 +1,11 @@
 import Search from './Search';
 import SearchBox from './SearchBox';
+import Profile from './Profile';
+import SummonerInfo from './SummonerInfo';
 
 export default {
   Search,
-  SearchBox
+  SearchBox,
+  Profile,
+  SummonerInfo
 };
