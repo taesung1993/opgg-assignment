c855029ca8df9980d07abf0f6825840817784142
taesung1993 <cheonyulin@gmail.com>
2022-08-20 09:55:21 +0900

    update HeaderTest

--- src/components/organisms/tests/Header.test.tsx
+++ src/components/organisms/tests/Header.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { RecoilRoot } from 'recoil';
 import Header from '../Header';
 
 test('input "Hide on bush" in Search Input', async () => {
-  render(<Header />);
+  render(
+    <RecoilRoot>
+      <Header />
+    </RecoilRoot>
+  );
   const searchInput = screen.getByPlaceholderText('소환사명,챔피언…');
   await userEvent.type(searchInput, 'Hide on bush');
   expect(searchInput).toHaveDisplayValue('Hide on bush');
