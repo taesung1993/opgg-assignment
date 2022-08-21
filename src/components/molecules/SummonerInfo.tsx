eb85783b386a5437d1a6da3228f4102cab4e7818
taesung1993 <cheonyulin@gmail.com>
2022-08-21 20:36:06 +0900

    apply summoner leages component

--- src/components/molecules/SummonerInfo.tsx
+++ src/components/molecules/SummonerInfo.tsx
@@ -1,7 +1,38 @@
+import { useMemo } from 'react';
+import { useRecoilValue } from 'recoil';
+import { AsyncData } from '../../models/interfaces/AsyncData';
+import { ILeague } from '../../models/interfaces/League';
+import States from '../../states';
+import Atoms from '../atoms';
+
 export default function SummonerInfo() {
+  const { status, data } = useRecoilValue(States.SummonerProfile);
+  const leagues = useMemo<AsyncData<ILeague[]>>(() => {
+    if (status === 'loading') {
+      return {
+        status,
+        data
+      };
+    }
+
+    if (status === 'error') {
+      return {
+        status,
+        data
+      };
+    }
+
+    return {
+      status,
+      data: data.leagues
+    };
+  }, [status]);
+
   return (
     <section className='flex w-full max-w-opgg mx-auto'>
-      <section className='w-[18.75rem] mr-2.5'>1</section>
+      <section className='w-[18.75rem] mr-2.5 pt-2.5'>
+        <Atoms.SummonerLeagues {...leagues} />
+      </section>
       <section className='flex-1'>2</section>
     </section>
   );
