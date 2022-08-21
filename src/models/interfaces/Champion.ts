a8f34daba3f871a10f0bd1befb60e86684f896b1
taesung1993 <cheonyulin@gmail.com>
2022-08-21 22:56:54 +0900

    add Champion type

---
+++ src/models/interfaces/Champion.ts
@@ -0,0 +1,14 @@
+export interface IChampion {
+  id: number;
+  key: string;
+  name: string;
+  imageUrl: string;
+  games: number;
+  kills: number;
+  deaths: number;
+  assists: number;
+  wins: number;
+  losses: number;
+  cs: number;
+  rank: number;
+}
