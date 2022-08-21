d3bc105965f8fe60e8b156942a843600e9e28ea7
taesung1993 <cheonyulin@gmail.com>
2022-08-21 22:32:03 +0900

    add divider Option

--- src/components/atoms/TabMenus.tsx
+++ src/components/atoms/TabMenus.tsx
@@ -4,6 +4,7 @@ interface Style {
   tabs?: {
     width?: string;
     height?: string;
+    divider?: string;
   };
   buttonDefault?: {
     [key: string]: string;
@@ -29,12 +30,17 @@ export default function TabMenus<T extends { id: string; title: string }>({
   const selectedId = selectedItem.id;
   const buttonDefault = style?.buttonDefault || {
     background: '#e3e3e3',
-    color: '#9c9c9c'
+    color: '#9c9c9c',
+    fontSize: '0.875rem',
+    lineHeight: '1.25rem'
   };
   const buttonActive = style?.buttonActive || {
     background: '#fff',
-    color: '#4a4a4a'
+    color: '#4a4a4a',
+    fontSize: '0.875rem',
+    lineHeight: '1.25rem'
   };
+  const isDividerOption = !!style?.tabs?.divider;
 
   return (
     <nav
@@ -44,15 +50,26 @@ export default function TabMenus<T extends { id: string; title: string }>({
         height: style?.tabs?.height || '2.5rem'
       }}>
       <ul className='flex items-center w-full h-full'>
-        {tabItems.map((tabItem) => (
-          <li className='flex-1 w-full h-full' key={tabItem.id}>
-            <button
-              className='w-full h-full flex justify-center items-center text-sm'
-              style={selectedId === tabItem.id ? buttonActive : buttonDefault}
-              onClick={() => setSelectedItem(tabItem)}>
-              {tabItem.title}
-            </button>
-          </li>
+        {tabItems.map((tabItem, index) => (
+          <>
+            {isDividerOption && index ? (
+              <div
+                className='w-[1px] h-full'
+                style={{
+                  backgroundColor: style?.tabs?.divider || 'transparent'
+                }}></div>
+            ) : (
+              <></>
+            )}
+            <li className='flex-1 w-full h-full' key={tabItem.id}>
+              <button
+                className='w-full h-full flex justify-center items-center'
+                style={selectedId === tabItem.id ? buttonActive : buttonDefault}
+                onClick={() => setSelectedItem(tabItem)}>
+                {tabItem.title}
+              </button>
+            </li>
+          </>
         ))}
       </ul>
     </nav>
