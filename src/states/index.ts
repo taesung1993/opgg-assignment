da89be5c7048ef32c65823b12823c66d49d81db1
taesung1993 <cheonyulin@gmail.com>
2022-08-21 16:06:41 +0900

    add SummonerProfile state

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,9 +1,11 @@
 import SearchedSummoner from './SearchedSummoner';
 import SearchKeyword from './SearchKeyword';
 import SummonerName from './SummonerName';
+import SummonerProfile from './SummonerProfile';
 
 export default {
   SearchKeyword,
   SearchedSummoner,
-  SummonerName
+  SummonerName,
+  SummonerProfile
 };
