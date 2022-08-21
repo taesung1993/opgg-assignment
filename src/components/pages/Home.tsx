dbc0f2da5b6a26152dfd39d9203430cdb99f6129
taesung1993 <cheonyulin@gmail.com>
2022-08-21 23:16:31 +0900

    useQuery hooks 대신 useQuries를 이용하여 한번에 받아오도록 설정

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,14 +1,57 @@
 import Templates from '../templates';
 import Organisms from '../organisms';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import States from '../../states';
-import { useSummoner } from '../../hooks/useSummoner';
-import { useGameWinsRate } from '../../hooks/useGameWinsRate';
+import { useQueries } from 'react-query';
+import {
+  getGameWinsRateInQueryFn,
+  getSummonerInQueryFn
+} from '../../controllers/summonerController';
+import { ISummoner } from '../../models/interfaces/Summoner';
+import { AxiosError } from 'axios';
+import { IGameWinsRate } from '../../models/interfaces/GameWinsRate';
 
 export default function Home() {
+  const setProfile = useSetRecoilState(States.SummonerProfile);
+  const setGameWinsRate = useSetRecoilState(States.GameWinsRate);
   const summonerName = useRecoilValue(States.SummonerName);
-  const _getSummoner = useSummoner(summonerName);
-  const _getGameWinsRate = useGameWinsRate(summonerName);
+
+  useQueries([
+    {
+      queryKey: ['summoner', summonerName],
+      queryFn: getSummonerInQueryFn,
+      refetchOnWindowFocus: false,
+      onSuccess: (data: ISummoner) => {
+        setProfile({
+          status: 'success',
+          data
+        });
+      },
+      onError: (err: any) => {
+        setProfile({
+          status: 'error',
+          data: err.message
+        });
+      }
+    },
+    {
+      queryKey: ['gameWinsRate', summonerName],
+      queryFn: getGameWinsRateInQueryFn,
+      refetchOnWindowFocus: false,
+      onSuccess: (data: IGameWinsRate) => {
+        setGameWinsRate({
+          status: 'success',
+          data
+        });
+      },
+      onError: (err: any) => {
+        setGameWinsRate({
+          status: 'error',
+          data: err.message
+        });
+      }
+    }
+  ]);
 
   return (
     <Templates.Nested>
