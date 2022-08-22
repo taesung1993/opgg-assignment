6006513699b9d44ff30f8b79f68ec8c42f09960d
taesung1993 <cheonyulin@gmail.com>
2022-08-22 20:22:50 +0900

    add filterGames

--- src/controllers/matchesController.ts
+++ src/controllers/matchesController.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { QueryFunctionContext } from 'react-query';
+import { IGame } from '../models/interfaces/Game';
 import { IMatches } from '../models/interfaces/Matches';
 
-const getMatchesInQueryFn = async ({
+export const getMatchesInQueryFn = async ({
   queryKey
 }: QueryFunctionContext): Promise<IMatches> => {
   const [_, name] = queryKey;
@@ -14,4 +15,11 @@ const getMatchesInQueryFn = async ({
   } catch (err: any) {
     throw new Error(err.message);
   }
+};
+
+export const filterGames = (
+  gameType: '솔랭' | '일반' | '자유 5:5 랭크',
+  games: IGame[]
+) => {
+  return games.filter((g) => g.gameType === gameType);
 };
