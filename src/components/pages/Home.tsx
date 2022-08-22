4a9e98ea24660d10a694e693dad1ad1b033665b8
taesung1993 <cheonyulin@gmail.com>
2022-08-22 20:26:04 +0900

    add matches query

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,18 +3,21 @@ import Organisms from '../organisms';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import States from '../../states';
 import { useQueries } from 'react-query';
-import {
-  getMostInfo,
-  getSummonerInQueryFn
-} from '../../controllers/summonerController';
+import { getSummonerInQueryFn } from '../../controllers/summonerController';
 import { ISummoner } from '../../models/interfaces/Summoner';
 import { IMostInfo } from '../../models/interfaces/MostInfo';
-import { IChampion } from '../../models/interfaces/Champion';
-import { IChampionWinRate } from '../../models/interfaces/ChampionWinRate';
+import { getMatchesInQueryFn } from '../../controllers/matchesController';
+import {
+  getMostInfo,
+  sortChampions,
+  sortChampionWinRate
+} from '../../controllers/mostInfoController';
+import { IMatches } from '../../models/interfaces/Matches';
 
 export default function Home() {
   const setProfile = useSetRecoilState(States.SummonerProfile);
   const setMostInfo = useSetRecoilState(States.MostInfo);
+  const setMatches = useSetRecoilState(States.Matches);
   const summonerName = useRecoilValue(States.SummonerName);
 
   useQueries([
@@ -45,12 +48,29 @@ export default function Home() {
           data: {
             ...data,
             champions: data.champions.sort(sortChampions),
-            recentWinRate: data.recentWinRate.sort(sortRecentWinRate)
+            recentWinRate: data.recentWinRate.sort(sortChampionWinRate)
           }
         });
       },
       onError: (err: any) => {
         setMostInfo({
+          status: 'error',
+          data: err.message
+        });
+      }
+    },
+    {
+      queryKey: ['matches', summonerName],
+      queryFn: getMatchesInQueryFn,
+      refetchOnWindowFocus: false,
+      onSuccess: (data: IMatches) => {
+        setMatches({
+          status: 'success',
+          data
+        });
+      },
+      onError: (err: any) => {
+        setMatches({
           status: 'error',
           data: err.message
         });
@@ -63,36 +83,4 @@ export default function Home() {
       <Organisms.Main />
     </Templates.Nested>
   );
-}
-
-function sortChampions(championA: IChampion, championB: IChampion): number {
-  const { games: gamesA } = championA;
-  const { games: gamesB } = championB;
-
-  if (gamesA > gamesB) {
-    return -1;
-  }
-  if (gamesA === gamesB) {
-    return 0;
-  }
-  return 1;
-}
-
-function sortRecentWinRate(
-  recentWinRateA: IChampionWinRate,
-  recentWinRateB: IChampionWinRate
-) {
-  const { wins: winsA, losses: lossesA } = recentWinRateA;
-  const { wins: winsB, losses: lossesB } = recentWinRateB;
-
-  const totalA = winsA + lossesA;
-  const totalB = winsB + lossesB;
-
-  if (totalA > totalB) {
-    return -1;
-  }
-  if (totalA === totalB) {
-    return 0;
-  }
-  return 1;
 }
