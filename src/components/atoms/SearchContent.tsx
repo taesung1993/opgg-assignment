7f18a3b67c1c13d270de5f7f5c60b93eaadb96a9
taesung1993 <cheonyulin@gmail.com>
2022-08-24 16:37:45 +0900

    클릭 했을 경우, 해당 소환사 정보에 대한 화면을 바꾼다

--- src/components/atoms/SearchContent.tsx
+++ src/components/atoms/SearchContent.tsx
@@ -8,6 +8,8 @@ import {
 import { ISummoner } from '../../models/interfaces/Summoner';
 import { RiCloseFill } from 'react-icons/ri';
 import { useCallback, useMemo, useState, MouseEvent } from 'react';
+import { useSetRecoilState } from 'recoil';
+import States from '../../states';
 
 interface Props {
   type: 'latest' | 'liked';
@@ -23,9 +25,20 @@ export default function SeachContent({ type }: Props) {
 
 function LatestList() {
   const [forceUpdate, setForceUpdate] = useState(false);
+  const setSummonerName = useSetRecoilState(States.SummonerName);
+
   const latestList = useMemo(() => {
     return getSummonerInLocal('latest');
   }, [forceUpdate]);
+  const changeScreen = useCallback((event: MouseEvent) => {
+    if (event.stopPropagation) {
+      event.stopPropagation();
+    }
+    const name = event.currentTarget.textContent;
+    if (name) {
+      setSummonerName(name);
+    }
+  }, []);
 
   const onLiked = useCallback(
     (event: MouseEvent, summoner: ISummoner) => {
@@ -64,7 +77,12 @@ function LatestList() {
             <div className='text-xs text-white font-bold bg-opgg py-[0.125rem] px-1 rounded-sm mr-4'>
               KR
             </div>
-            <div className='flex-1'>{s.name}</div>
+            <div
+              className='flex-1
+            cursor-pointer hover:text-opgg'
+              onClick={changeScreen}>
+              {s.name}
+            </div>
             <div className='flex items-center'>
               <button
                 onClick={(event) => onLiked(event, s)}
@@ -99,9 +117,21 @@ function LatestList() {
 
 function LikedList() {
   const [forceUpdate, setForceUpdate] = useState(false);
+  const setSummonerName = useSetRecoilState(States.SummonerName);
+
   const likedList = useMemo(() => {
     return getSummonerInLocal('liked');
   }, [forceUpdate]);
+
+  const changeScreen = useCallback((event: MouseEvent) => {
+    if (event.stopPropagation) {
+      event.stopPropagation();
+    }
+    const name = event.currentTarget.textContent;
+    if (name) {
+      setSummonerName(name);
+    }
+  }, []);
 
   const onRemove = useCallback(
     (event: MouseEvent, summoner: ISummoner) => {
@@ -123,7 +153,11 @@ function LikedList() {
             <div className='text-xs text-white font-bold bg-opgg py-[0.125rem] px-1 rounded-sm mr-4'>
               KR
             </div>
-            <div className='flex-1'>{s.name}</div>
+            <div
+              className='flex-1 hover:text-opgg cursor-pointer'
+              onClick={changeScreen}>
+              {s.name}
+            </div>
             <div className='flex items-center'>
               <button
                 className='ml-2'
